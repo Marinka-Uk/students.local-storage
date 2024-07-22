63766c994a4b95364a9304c0c683f28da21bf823
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-07-22 20:30:25 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const formEl = document.getElementById('studentForm')
-const tableRef = document.getElementById('studentsTable').querySelector('tbody')
+// const formEl = document.getElementById('studentForm')
+// const tableRef = document.getElementById('studentsTable').querySelector('tbody')
 
-formEl.addEventListener('submit', onSubmit)
+// formEl.addEventListener('submit', onSubmit)
 
-function onSubmit(event){
-    event.preventDefault()
+// function onSubmit(event){
+//     event.preventDefault()
 
-   const form = event.currentTarget
+//    const form = event.currentTarget
                    
 
-   const student = {
-    id: Date.now()
-    firstName: 
-    lastName: 
-    age:
-    course:
-    faculty:
+//    const student = {
+//     id: Date.now()
+//     firstName: 
+//     lastName: 
+//     age:
+//     course:
+//     faculty:
     
-   }
-   event.reset()
-}
+//    }
+//    event.reset()
+// }
